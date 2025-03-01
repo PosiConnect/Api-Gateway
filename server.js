ff49bb607d59c5e6bb97e0f52015ac52522cb12b
JClerve <joesonclerve9@gmail.com>
2025-03-01 12:13:29 +0530

    User service Proxy

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  "api/user",
+  createProxyMiddleware({ target: "http://localhost:3000", changeorigin: true })
+);
+
 app.listen(PORT, (req, res) => {
   console.log(`API Gateway is running on ${PORT}`);
 });
